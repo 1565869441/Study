be55de51edc2cff446ae5ec6994b6d7d5b04c737
cai <1565869441@qq.com>
2019-09-06 16:05:55 +0800

    提交代码

--- src/main/java/io/renren/StudyApplication.java
+++ src/main/java/io/renren/StudyApplication.java
@@ -13,10 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class RenrenApplication {
+public class StudyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RenrenApplication.class, args);
+		SpringApplication.run(StudyApplication.class, args);
 	}
 
 }
